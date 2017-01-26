8b363eed883f3280db69241a045c9890a11c2c7a
jedi17 <jedi17@a5s104mac45.fib.upc.es>
2017-01-26 14:44:09 +0100

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by jedi17 on 26/1/17.
+//  Copyright © 2017 jedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
