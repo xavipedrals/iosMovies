18c1e1878d3957b76fcf3e749cf2b344ced91410
jedi17 <jedi17@a5s104mac45.fib.upc.es>
2017-02-03 15:02:44 +0100

    Added some UI tests

--- MoviesAppTests/APITest.swift
+++ MoviesAppTests/APITest.swift
@@ -11,25 +11,52 @@ import XCTest
 
 class APITest: XCTestCase {
     
-//    override func setUp() {
-//        super.setUp()
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//    
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
-    
     func testAllMovies() {
-        NetworkController.getMovies(offset: 0, completion: {
+        let moviesExpectation = expectation(description: "The API is responding correctly")
+        NetworkController.getMovies(offset: 0, genre: .all, completion: {
             movies in
             XCTAssert(movies.count != 0, "Movies array should have some elements")
             for i in 0...3 {
                 let movie = movies[i]
+                XCTAssertNotNil(movie.title)
+                XCTAssertNotNil(movie.summary)
+                XCTAssertNotNil(movie.image)
                 XCTAssertTrue(movie.title != "", "Movie title should not be empty")
                 XCTAssertTrue(movie.summary != "", "Movie summary should not be empty")
-                XCTAssertTrue(movie.director == "Sida", "Movie should have a director")
+                XCTAssertTrue(movie.image != "", "Movie image shoul not be empty")
+            }
+            moviesExpectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10, handler: {
+            error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        })
+    }
+    
+    func testMoviesWithGenre() {
+        let moviesExpectation = expectation(description: "The API is responding correctly")
+        NetworkController.getMovies(offset: 0, genre: .anime, completion: {
+            movies in
+            XCTAssert(movies.count != 0, "Movies array should have some elements")
+            for i in 0...3 {
+                let movie = movies[i]
+                XCTAssertNotNil(movie.title)
+                XCTAssertNotNil(movie.summary)
+                XCTAssertNotNil(movie.image)
+                XCTAssertTrue(movie.title != "", "Movie title should not be empty")
+                XCTAssertTrue(movie.summary != "", "Movie summary should not be empty")
+                XCTAssertTrue(movie.image != "", "Movie image shoul not be empty")
+            }
+            moviesExpectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10, handler: {
+            error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         })
     }
